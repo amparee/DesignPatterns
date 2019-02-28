75d1a171941df25e2bc1ff6f04cedcd7ff1a253d
pabloservile <pabloservile@gmail.com>
2019-02-28 11:51:59 -0300

    State

---
+++ src/State/State.java
@@ -0,0 +1,10 @@
+package State;
+
+public interface State {
+
+    void insertQuarter();
+    void ejectQuarter();
+    void turnCrank();
+    void dispense();
+
+}
---
+++ src/State/WinnerState.java
@@ -0,0 +1,50 @@
+package State;
+
+public class WinnerState implements State {
+
+    GumballMachine gumballMachine;
+
+    public WinnerState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public GumballMachine getGumballMachine() {
+        return gumballMachine;
+    }
+
+    public void setGumballMachine(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+
+    }
+
+    @Override
+    public void ejectQuarter() {
+
+    }
+
+    @Override
+    public void turnCrank() {
+
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("YOU’RE A WINNER! You get two gumballs for your quarter");
+        gumballMachine.releaseBall();
+        if (gumballMachine.getCount() == 0) {
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        } else {
+            gumballMachine.releaseBall();
+            if (gumballMachine.getCount() > 0) {
+                gumballMachine.setState(gumballMachine.getNoQuarterState());
+            } else {
+                System.out.println("Oops, out of gumballs!");
+                gumballMachine.setState(gumballMachine.getSoldOutState());
+            }
+        }
+    }
+}
